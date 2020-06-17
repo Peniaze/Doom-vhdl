cc07bd0c5837ac3f8ae9362d2b633b16f5911106
Peniaze <42100202+Peniaze@users.noreply.github.com>
2020-06-17 22:30:12 +0200

    Add files via upload

--- pascal test/unit1.pas
+++ pascal test/unit1.pas
@@ -10,6 +10,7 @@ uses
 
 type
 
+
   { TForm1 }
 
   TForm1 = class(TForm)
@@ -29,18 +30,51 @@ type
 
   end;
 
+const mapsize = 99;
+
 var
+
   Form1: TForm1;
   pangle: double;
   px,py: integer;
+  map:array [0..mapsize] of array [0..mapsize] of boolean;
 
 implementation
+
 
 
 {$R *.lfm}
 
 
 { TForm1 }
+procedure loadmap;
+var
+  MyBitmap: TBitmap;
+   i, j: integer;
+begin
+  MyBitmap := TBitmap.Create;
+  try
+    // Load from disk
+    MyBitmap.LoadFromFile('map.bmp');
+
+    // Here you can use MyBitmap.Canvas to read/write to/from the image
+
+    for i:=0 to mapsize do
+    begin
+        for j:= 0 to mapsize do
+        begin
+            if (mybitmap.Canvas.Pixels[i,j] = $000000) then
+               map[i,j]:= true;
+        end;
+    end;
+
+
+  finally
+    MyBitmap.Free;
+  end;
+end;
+
+
 
 procedure TForm1.Image1Click(Sender: TObject);
 var i, j: integer;
@@ -48,35 +82,27 @@ var i, j: integer;
     testposx: double;
     testposy: double;
 
-    distance: integer;
+    distance: double;
+    int_distance: integer;
     testangle: double;
     tvectx: double;
     tvecty: double;
+    vec_length: double;
     hitwall: boolean;
-
-    map:array [0..49] of array [0..49] of boolean;
 
 begin
 
-
-     // vytvorenie mapy (50x50) steny na krajoch
-     for i:= 0 to 49 do begin
-         for j:= 0 to 49 do begin
-             if (i<>0) and (i<>49) and (j<>0) and (j<>49) then
-               map[i, j]:= false
-             else
-               map[i, j]:= true;
-         end;
-     end;
+     loadmap;
 
      //raycasting pre kazdy pixel sirky (640)
      for i:= -320 to 320 do
      begin
-          testangle := pangle + i*0.5236/320;     //FOV -> 60deg
+          testangle := pangle + i*0.2618/320;     //FOV -> 30deg
 
           //vektor posuvu testovacieho lucu
           tvectx:= sin(testangle);
           tvecty:= cos(testangle);
+	  vec_length:= sqrt(tvectx*tvectx+tvecty*tvecty);
 
           //pozicia testovacieho lucu
           testposx:= px;
@@ -84,25 +110,27 @@ begin
 
           hitwall := false;
           distance := 0;
+	  int_distance:= 0;
 
 
-          while (not hitwall) and (distance < 100) do
+          while (not hitwall) and (distance < 1000) do
             begin
                  testposx += tvectx;
                  testposy += tvecty;
-                 distance += 1;
+                 distance += vec_length;
                  if map[round(testposx), round(testposy)] then
                     hitwall:= true;
             end;
 
+	    int_distance:= round(distance);
 
           if hitwall then begin
             //tienovanie
-            image1.canvas.Pen.Color:=$010000*2*distance+$000100*2*distance+$000001*2*distance;
+            image1.canvas.Pen.Color:=$8F0000-$010000*int_distance+$008F00-$000100*int_distance+$00008F-$000001*int_distance;
 
             //kreslenie zvislej ciary pre kazdy pixel sirky
-            image1.canvas.MoveTo(i+320,distance*3);
-            image1.canvas.LineTo(i+320, 480-distance*3);
+            image1.canvas.MoveTo(i+320,int_distance);
+            image1.canvas.LineTo(i+320, 480-int_distance);
           end;
 
 
