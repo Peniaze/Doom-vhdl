e675c28275c005ae9485aaa07e8755ba4d51671c
Peniaze <42100202+Peniaze@users.noreply.github.com>
2020-06-25 15:29:42 +0200

    Už to nevyzerá ako ***
    -- Zmenená veľkosť mapky na 20x20
    -- Veľkosť siete na pohyb zmenená na 500x500
    -- Pridaná grafická mapka na lepšiu orientáciu
    -- Opravený spôsob kreslenia na základe vzdialenosti vektoru lúču

---
+++ pascal test/readme.txt
@@ -0,0 +1,11 @@
+Návod na použitie:
+
+Nefunguje mi z nejakého dôvodu v pascale ovládanie klávesnicou (fakt fakt
+neviem prečo)
+
+1.  Pri zapnutí je treba kliknúť na malý "edit" v pravo, čo vyzerá ako okno
+    na vkladanie textu.
+2.  Následne fungujú klávesy WASD
+    klávesy A,D otáčajú obrazovku
+3.  Klávesy na pohyb treba vždy chvíľu podržať (toto je kvôli spôsobu 
+    vkladania textu vo windowse [prvý delay je 500ms a potom po 30ms])
--- pascal test/unit1.pas
+++ pascal test/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  StdCtrls, math;
+  StdCtrls, LCLType;
 
 type
 
@@ -18,10 +18,13 @@ type
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
+    Edit1: TEdit;
     Image1: TImage;
+    Image2: TImage;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
+    procedure Edit1Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure Image1Click(Sender: TObject);
   private
@@ -30,12 +33,17 @@ type
 
   end;
 
-const mapsize = 99;
+const mapsize = 19;
 
 var
 
   Form1: TForm1;
   pangle: double;
+
+  // Suradnice px,py .... od 0 do 500 (mapa 20x20),
+  // pre zmenu riadok 128 --> max(testposx)/[x] = mapsize,
+  // ovplyvnuje to aj detail pocitania vzdialenosti lucov (mozno bude treba zmensit)
+  // suvisi aj riadok 134 --> vacsie suradnice = vacsia vzdialenost
   px,py: integer;
   map:array [0..mapsize] of array [0..mapsize] of boolean;
 
@@ -52,10 +60,10 @@ var
   MyBitmap: TBitmap;
    i, j: integer;
 begin
-  MyBitmap := TBitmap.Create;
+  MyBitmap := TBitmap.Create();
   try
     // Load from disk
-    MyBitmap.LoadFromFile('map.bmp');
+    MyBitmap.LoadFromFile('map2.bmp');
 
     // Here you can use MyBitmap.Canvas to read/write to/from the image
 
@@ -67,6 +75,7 @@ begin
                map[i,j]:= true;
         end;
     end;
+    Form1.image2.Canvas.StretchDraw(form1.Image2.ClientRect,mybitmap);
 
 
   finally
@@ -77,13 +86,13 @@ end;
 
 
 procedure TForm1.Image1Click(Sender: TObject);
-var i, j: integer;
+var i: integer;
 
     testposx: double;
     testposy: double;
 
     distance: double;
-    int_distance: integer;
+    height_ceiling: integer;
     testangle: double;
     tvectx: double;
     tvecty: double;
@@ -97,11 +106,11 @@ begin
      //raycasting pre kazdy pixel sirky (640)
      for i:= -320 to 320 do
      begin
-          testangle := pangle + i*0.2618/320;     //FOV -> 30deg
+          testangle := pangle + i*0.5236/320;     //FOV -> 60deg
 
           //vektor posuvu testovacieho lucu
-          tvectx:= sin(testangle);
-          tvecty:= cos(testangle);
+          tvectx:= sin(testangle)/10;
+          tvecty:= cos(testangle)/10;
 	  vec_length:= sqrt(tvectx*tvectx+tvecty*tvecty);
 
           //pozicia testovacieho lucu
@@ -110,7 +119,7 @@ begin
 
           hitwall := false;
           distance := 0;
-	  int_distance:= 0;
+	  height_ceiling:= 0;
 
 
           while (not hitwall) and (distance < 1000) do
@@ -118,19 +127,20 @@ begin
                  testposx += tvectx;
                  testposy += tvecty;
                  distance += vec_length;
-                 if map[round(testposx), round(testposy)] then
+                 if map[round(testposx/25), round(testposy/25)] then
                     hitwall:= true;
             end;
 
-	    int_distance:= round(distance);
+	    height_ceiling:= round(240-10000/distance);
+            if height_ceiling<0 then height_ceiling:=0;
 
           if hitwall then begin
             //tienovanie
-            image1.canvas.Pen.Color:=$8F0000-$010000*int_distance+$008F00-$000100*int_distance+$00008F-$000001*int_distance;
+            image1.canvas.Pen.Color:=$AF0000-$010000*height_ceiling+$00AF00-$000100*height_ceiling+$0000AF-$000001*height_ceiling;
 
             //kreslenie zvislej ciary pre kazdy pixel sirky
-            image1.canvas.MoveTo(i+320,int_distance);
-            image1.canvas.LineTo(i+320, 480-int_distance);
+            image1.canvas.MoveTo(i+320,height_ceiling);
+            image1.canvas.LineTo(i+320, 480-height_ceiling);
           end;
 
 
@@ -142,14 +152,18 @@ begin
 
 end;
 
+
+
+
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   image1.canvas.fillrect(clientrect);
 
   //nastavenie globalnych premennych pri starte
    pangle := 0;
-   px := 25;
-   py := 25;
+   px := 250;
+   py := 250;
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -176,6 +190,41 @@ begin
       image1.canvas.fillrect(clientrect);
      Image1Click(nil);
 end;
+
+procedure TForm1.Edit1Change(Sender: TObject);
+begin
+     if edit1.Text = 'w' then
+     begin
+    px := round(px + sin(pangle)*5);
+    py := round(py + cos(pangle)*5);
+    image1.canvas.fillrect(clientrect);
+    Image1Click(nil);
+    end;
+     if edit1.Text = 's' then
+     begin
+    px := round(px - sin(pangle));
+    py := round(py - cos(pangle));
+    image1.canvas.fillrect(clientrect);
+    Image1Click(nil);
+    end;
+     if edit1.Text = 'a' then
+    begin
+    //posunutie pohladu dolava
+    pangle := pangle - 0.1;
+    image1.canvas.fillrect(clientrect);
+    Image1Click(nil);
+    end;
+     if edit1.Text = 'd' then
+     begin
+    //posunutie pohladu doprava
+    pangle := pangle + 0.1;
+    image1.canvas.fillrect(clientrect);
+    Image1Click(nil);
+    end;
+     edit1.Text:='';
+     image2.canvas.Rectangle(round(px/5),round(py/5),round(px/5+2),round(py/5+2));
+end;
+
 
 end.
 
